d2343dc3fd81d8757912b1a3e627a4dbf17b399c
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-09 18:41:57 -0300

    upload project

---
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/DesafioItausApplication.java
@@ -0,0 +1,13 @@
+package com.mak.desafio_itaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItausApplication.class, args);
+	}
+
+}
