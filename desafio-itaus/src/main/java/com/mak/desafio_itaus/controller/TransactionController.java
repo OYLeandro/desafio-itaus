610ba9589245a1a04b0612f286b5941b74a1f57c
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-09 22:42:13 -0300

    adding method to file

--- desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/TransactionController.java
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/TransactionController.java
@@ -1,7 +1,30 @@
 package com.mak.desafio_itaus.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mak.desafio_itaus.business.TransactionService;
+import com.mak.desafio_itaus.controller.dtos.TransactionResquestDto;
+import com.mak.desafio_itaus.infrastructure.exceptions.UnprocessableEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/transacao")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Void> addTransaction(@RequestBody TransactionResquestDto dto){
+            transactionService.addTransaction(dto);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTransactions(){
+        transactionService.deleteTransactions();
+        return ResponseEntity.ok().build();
+    }
 }
