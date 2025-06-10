6d4bac44047daf084fd918b87a01502b255495fd
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-10 00:03:36 -0300

    adding status code to files

--- desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/StatisticController.java
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/StatisticController.java
@@ -3,6 +3,9 @@ package com.mak.desafio_itaus.controller;
 import com.mak.desafio_itaus.business.StatisticService;
 import com.mak.desafio_itaus.controller.dtos.StatisticRequestDto;
 import com.mak.desafio_itaus.controller.dtos.TransactionResquestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,12 @@ public class StatisticController {
     private final StatisticService statisticService;
 
     @GetMapping
+    @Operation(description = "EndPoint responsavel por buscar transação!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na busca da estastitica das transações"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<StatisticRequestDto> calculeteTransactionStatistic(@RequestParam
                                                                              (value = "Intervalo de busca",
                                                                              required = false,
--- desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/TransactionController.java
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/TransactionController.java
@@ -3,11 +3,17 @@ package com.mak.desafio_itaus.controller;
 import com.mak.desafio_itaus.business.TransactionService;
 import com.mak.desafio_itaus.controller.dtos.TransactionResquestDto;
 import com.mak.desafio_itaus.infrastructure.exceptions.UnprocessableEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jdk.jfr.Description;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.management.Descriptor;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +22,13 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
+    @Operation(description = "EndPoint responsavel por adicionar transação!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação feita com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requesitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> addTransaction(@RequestBody TransactionResquestDto dto){
             transactionService.addTransaction(dto);
 
@@ -23,6 +36,12 @@ public class TransactionController {
     }
 
     @DeleteMapping
+    @Operation(description = "EndPoint responsavel por deletar transação!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transação deletada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deleteTransactions(){
         transactionService.deleteTransactions();
         return ResponseEntity.ok().build();
