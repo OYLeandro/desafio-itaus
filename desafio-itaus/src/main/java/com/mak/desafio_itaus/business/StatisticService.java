26b9ef163fb62bf0614c8809897c8c12833ab28a
Leandro <163356969+OYLeandro@users.noreply.github.com>
2025-06-09 22:15:01 -0300

    creating methods in StatisticService file and Update StatisticRequestDto file

--- desafio-itaus/src/main/java/com/mak/desafio_itaus/business/StatisticService.java
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/business/StatisticService.java
@@ -1,13 +1,28 @@
 package com.mak.desafio_itaus.business;
 
+import com.mak.desafio_itaus.controller.dtos.StatisticRequestDto;
 import com.mak.desafio_itaus.controller.dtos.TransactionResquestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticService {
     private final TransactionService transactionService;
+
+    public StatisticRequestDto calculateTransactionStatistic(Integer timeInterval){
+        List<TransactionResquestDto> transaction = transactionService.finByTransaction(timeInterval);
+
+        DoubleSummaryStatistics transactionStatistic = transaction.stream()
+                .mapToDouble(TransactionResquestDto::valor).summaryStatistics();
+
+        return new StatisticRequestDto(transactionStatistic.getCount(),
+                transactionStatistic.getSum(),
+                transactionStatistic.getAverage(),
+                transactionStatistic.getMin(),
+                transactionStatistic.getMax());
+    }
 }
--- desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/dtos/StatisticRequestDto.java
+++ desafio-itaus/src/main/java/com/mak/desafio_itaus/controller/dtos/StatisticRequestDto.java
@@ -1,6 +1,6 @@
 package com.mak.desafio_itaus.controller.dtos;
 
-public record StatisticRequestDto(Integer count,
+public record StatisticRequestDto(Long count,
                                   Double sum,
                                   Double avg,
                                   Double min,
